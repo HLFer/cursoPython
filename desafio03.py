0453b8b2b246dfa366976f6a378cfeb14e093528
Henrique <henriquedin@hotmail.com>
2018-05-11 16:51:56 -0300

    Curso Fundamentos em Python - Primeiros comandos

---
+++ desafio03.py
@@ -0,0 +1,5 @@
+print('Faça a soma de dois números: ')
+num1 = input('Digite o primeiro número: ')
+num2 = input('Digite o segundo número: ')
+soma = int(num1) + int(num2)
+print('O resultado da soma é: ',soma)
