e9bf66193cb5cf1a8faac9a61b9d6ed2f848f0e2
Henrique <henriquedin@hotmail.com>
2018-05-12 12:07:22 -0300

    Exercício 03 - Análise de tipos de dados

---
+++ Exercicios/ex003.py
@@ -0,0 +1,6 @@
+print('Programa para calcular a soma entre dois números.')
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1+num2
+print('A soma entre {} e {} é: {}'.format(num1,num2,soma))
+
